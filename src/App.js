9552f03db277c050bf8297ebf770e7da22fbbd1f
Anaya Punj <anjana.sharma@technogetic.com>
2023-10-29 11:04:34 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
+import React from "react";
 import Header from "./pages/layouts/Header";
 import Navbar from "./pages/layouts/Navbar";
 import Todo from "./pages/Todo";
 import Home from "./pages/Home";
 import Calculator from "./pages/Calculator";
 import { Route, Routes } from "react-router-dom";
-
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
       <Header />
       {/* <Navbar/> */}
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/todo" element={<Todo />}></Route>
         <Route path="/calculator" element={<Calculator />}></Route>
+        <Route path="/SignUp" element={<SignUp />}></Route>
       </Routes>
       {/* <Todo/> */}
     </>
